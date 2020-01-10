cac274ad5e755d9e5f9aafb8a796cb446836cf0e
Eugene Molari <eugelogic@gmail.com>
2020-01-10 21:39:08 +0000

    Add basic styling & move COST label to the bottom

--- list-messages.js
+++ list-messages.js
@@ -14,35 +14,36 @@ fetchMessages().then(info => {
         const niceDate = new Date(message.dateSent);
         sentItem.appendChild(document.createTextNode(niceDate));
         messageUl.appendChild(sentItem);
-        sentItem.insertAdjacentText('afterbegin', 'Sent on: ');
+        sentItem.insertAdjacentHTML('afterbegin', '<span class="message__label">Sent on:</span> ');
 
         const bodyItem = document.createElement('li');
         bodyItem.appendChild(document.createTextNode(message.body));
         messageUl.appendChild(bodyItem);
-        bodyItem.insertAdjacentText('afterbegin', 'Body: ');
-
-        const priceItem = document.createElement('li');
-        if (message.price === null) {
-            priceItem.appendChild(document.createTextNode('Cost: no charge'));
-            messageUl.appendChild(priceItem);
-        }
-        else {
-            const formattedPrice = message.price.substring(1);
-            priceItem.appendChild(document.createTextNode(formattedPrice));
-            messageUl.appendChild(priceItem);
-            priceItem.insertAdjacentText('afterbegin', 'Cost: £');
-        }
+        bodyItem.insertAdjacentHTML('afterbegin', '<span class="message__label">Body:</span> ');
 
         const statusItem = document.createElement('li');
         statusItem.appendChild(document.createTextNode(message.status));
         messageUl.appendChild(statusItem);
-        statusItem.insertAdjacentText('afterbegin', 'Status: ');
+        statusItem.insertAdjacentHTML('afterbegin', '<span class="message__label">Status:</span> ');
 
         const recepientItem = document.createElement('li');
         const partlyConcealedRecepient = message.to.substring(9);
         recepientItem.appendChild(document.createTextNode(partlyConcealedRecepient));
         messageUl.appendChild(recepientItem);
-        recepientItem.insertAdjacentText('afterbegin', 'Recepient number ending: ***')
+        recepientItem.insertAdjacentHTML('afterbegin', '<span class="message__label">Recepient:</span> ***')
+
+        const priceItem = document.createElement('li');
+        if (message.price === null) {
+            // priceItem.appendChild(document.createTextNode('<span class="message__label">Cost:</span> no charge'));
+            messageUl.appendChild(priceItem);
+            priceItem.insertAdjacentHTML('afterbegin', '<span class="message__label">Cost:</span> no charge');
+        }
+        else {
+            const formattedPrice = message.price.substring(1);
+            priceItem.appendChild(document.createTextNode(formattedPrice));
+            messageUl.appendChild(priceItem);
+            priceItem.insertAdjacentHTML('afterbegin', '<span class="message__label">Cost:</span> £');
+        }
 
         messagesList.appendChild(messageUl);
     });
